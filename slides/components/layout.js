92fc00f625498e1e0491f9d03ef6992019096073
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-06-27 09:47:46 +0800

    Update layout.js

--- slides/components/layout.js
+++ slides/components/layout.js
@@ -10,7 +10,7 @@ export default ({ children }) => (
       {children}
       <Fieldset.Footer style={{ padding: "3px 32px", height: "64px" }}>
         <Fieldset.Footer.Status>
-          <Code>Devhost 2021</Code>
+          <Code>CitroHacks</Code>
         </Fieldset.Footer.Status>
         <Fieldset.Footer.Actions>
           <Avatar src="https://github.com/sampoder.png" />
